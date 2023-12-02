1e19c3a2a9f454af16185830b83121b8baaa72b7
Ali Shafa <116679960+Alishafa123@users.noreply.github.com>
2023-12-03 02:27:15 +0500

    sign in configured with backend

--- dhubxon/src/components/Login_auth_Components/Login.js
+++ dhubxon/src/components/Login_auth_Components/Login.js
@@ -28,57 +28,101 @@ export default function Login(){
     }
 
    
-    const authenticateUser = async () => {
-        var email, pass;
-        email = loginState['email-address'];
-        pass = loginState['password'];
+    // const authenticateUser = async () => {
+    //     var email, pass;
+    //     email = loginState['email-address'];
+    //     pass = loginState['password'];
 
 
 
 
         
     
-        // try {
-        //     const clientResponse = await axios.post('http://127.0.0.1:5000/client/signIn', { email, pass });
+    //     try {
+    //         const clientResponse = await axios.post('http://127.0.0.1:5000/client/signIn', { email, pass });
     
-        //     if (clientResponse.status == 200) {
-        //         console.log("Client Sign-in successful!");
-        //         Swal.fire("Sign in Successfully!");
-        //         navigate('/jobview');
-        //         return;
-        //     }
+    //         if (clientResponse.status == 200) {
+    //             console.log("Client Sign-in successful!");
+    //             Swal.fire("Sign in Successfully!");
+    //             navigate('/jobview');
+    //             return;
+    //         }
     
-        //     const freelancerResponse = await axios.post('http://127.0.0.1:5000/freelancer/signIn', { email, pass });
+    //         const freelancerResponse = await axios.post('http://127.0.0.1:5000/freelancer/signIn', { email, pass });
     
-        //     if (freelancerResponse.status === 200) {
-        //         console.log("Freelancer Sign-in successful!");
-        //         Swal.fire("Sign in Successfully!");
-        //         navigate('/freelancerdashboard');
-        //     } else {
-        //         Swal.fire({
-        //             icon: "error",
-        //             title: "Oops...",
-        //             text: "User Not Found!",
-        //             footer: <a href="/signup">Create a new account</a>,
-        //         });
-        //         navigate('/login');
-        //     }
-        // } catch (error) {
-        //     console.error("Error during authentication:", error);
+    //         if (freelancerResponse.status === 200) {
+    //             console.log("Freelancer Sign-in successful!");
+    //             Swal.fire("Sign in Successfully!");
+    //             navigate('/freelancerdashboard');
+    //         } else {
+    //             Swal.fire({
+    //                 icon: "error",
+    //                 title: "Oops...",
+    //                 text: "User Not Found!",
+    //                 footer: <a href="/signup">Create a new account</a>,
+    //             });
+    //             navigate('/login');
+    //         }
+    //     } catch (error) {
+    //         console.error("Error during authentication:", error);
     
-        //     if (error.response) {
-        //         console.error("Server responded with non-2xx status:", error.response.data);
-        //         console.error("Status code:", error.response.status);
-        //     } else if (error.request) {
-        //         console.error("No response received from the server:", error.request);
-        //     } else {
-        //         console.error("Error setting up the request:", error.message);
-        //     }
+    //         if (error.response) {
+    //             console.error("Server responded with non-2xx status:", error.response.data);
+    //             console.error("Status code:", error.response.status);
+    //         } else if (error.request) {
+    //             console.error("No response received from the server:", error.request);
+    //         } else {
+    //             console.error("Error setting up the request:", error.message);
+    //         }
     
-        //     window.alert('Sign-in Failed');
-        //     navigate('/login');
-        // }
+    //         window.alert('Sign-in Failed');
+    //         navigate('/login');
+    //     }
+    // };
+
+
+
+    const authenticateUser = async () => {
+        const email = loginState['email-address'];
+        const pass = loginState['password'];
+    
+        try {
+            const clientResponse = await axios.post('http://127.0.0.1:5000/client/signIn', { email, pass });
+    
+            if (clientResponse.status === 200) {
+                console.log("Client Sign-in successful!");
+                Swal.fire("Sign in Successfully!");
+                navigate('/jobview');
+                return;
+            }
+        } catch (clientError) {
+            console.error("Client Authentication Error:", clientError);
+        }
+    
+        // If client login fails, try freelancer login
+        try {
+            const freelancerResponse = await axios.post('http://127.0.0.1:5000/freelancer/signIn', { email, pass });
+    
+            if (freelancerResponse.status === 200) {
+                console.log("Freelancer Sign-in successful!");
+                Swal.fire("Sign in Successfully!");
+                navigate('/freelancerdashboard');
+                return;
+            }
+        } catch (freelancerError) {
+            console.error("Freelancer Authentication Error:", freelancerError);
+        }
+    
+        // If both client and freelancer login fail
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "User Not Found!",
+            footer: <a href="/signup">Create a new account</a>,
+        });
+        navigate('/login');
     };
+    
     
     
 
