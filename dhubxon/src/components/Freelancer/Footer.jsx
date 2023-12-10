abff4dc981a99ad17ee179c4df9220547196a8c7
Zain Ul Abideen <79300060+zyn20@users.noreply.github.com>
2023-12-10 14:49:28 +0500

    Navbar and Footer Components updated

--- dhubxon/src/components/Freelancer/Footer.jsx
+++ dhubxon/src/components/Freelancer/Footer.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { motion } from 'framer-motion';
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-900 py-12">
+    <footer className="bg-blue-900 py-12 mt-auto">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap items-center justify-between">
           <div className="w-full lg:w-6/12 px-4 mb-8 lg:mb-0">
-            <h4 className="text-2xl font-semibold text-white mb-4">Stay Connected with D-hubXon</h4>
+            <h4 className="text-2xl font-semibold text-white mb-4">Stay Connected with D-HubXon</h4>
             <h5 className="text-lg mb-6 text-white">
               Follow us on social media for the latest updates and announcements.
             </h5>
@@ -20,97 +18,23 @@ const Footer = () => {
               whileTap={{ scale: 0.9 }}
               className="flex space-x-4"
             >
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 transition duration-300"
-              >
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              {/* Social Media Icons */}
+              <a href="#twitter">
+                <FontAwesomeIcon icon={faTwitter} className="text-white text-2xl" />
               </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 transition duration-300"
-              >
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+              <a href="#linkedin">
+                <FontAwesomeIcon icon={faLinkedin} className="text-white text-2xl" />
               </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 transition duration-300"
-              >
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 transition duration-300"
-              >
-                <FontAwesomeIcon icon={faEnvelope} size="2x" />
+              <a href="#instagram">
+                <FontAwesomeIcon icon={faInstagram} className="text-white text-2xl" />
               </a>
             </motion.div>
           </div>
           <div className="w-full lg:w-6/12 px-4 text-center">
             <div className="text-sm text-white font-semibold mb-4">
-              &copy; {new Date().getFullYear()} D-hubXon Freelance Marketplace
+              &copy; {new Date().getFullYear()} D-HubXon Freelance Marketplace
             </div>
-            <div className="text-sm text-white font-light mb-4">
-              Made with <FontAwesomeIcon icon={faHeart} className="text-red-500" /> by{' '}
-              <a
-                href="https://www.creative-tim.com?ref=njs-profile"
-                className="text-blueGray-500 hover:text-blueGray-800"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                D-hubXon Developers
-              </a>
-            </div>
-            <div className="flex justify-center space-x-6 flex-wrap">
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 mb-2"
-              >
-                Explore Courses
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 mb-2"
-              >
-                Join Community
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 mb-2"
-              >
-                About Us
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 mb-2"
-              >
-                Contact Us
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 mb-2"
-              >
-                Find Jobs
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 mb-2"
-              >
-                Explore Software Projects
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 mb-2"
-              >
-                Privacy Policy
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-300 mb-2"
-              >
-                Terms and Conditions
-              </a>
-            </div>
+            {/* Additional Footer Content */}
           </div>
         </div>
       </div>
